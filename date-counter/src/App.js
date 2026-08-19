3fab08822967e61fc8023bab730def96e8ff17c4
mahendra-purohit <mhpurohit90@gmail.com>
2026-08-19 13:23:16 +0530

    commits

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -1,4 +1,43 @@
+/////////////////////////////////////////first version////////////////
 import { useState } from "react";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <Counter />
+//     </div>
+//   );
+// }
+// function Counter() {
+//   const [count, setCount] = useState(0);
+//   const [step, setStep] = useState(1);
+//   const date = new Date();
+//   date.setDate(date.getDate() + count);
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={() => setStep((c) => c - 1)}>-</button>
+//         <span>Steps:{step}</span>
+//         <button onClick={() => setStep((c) => c + 1)}>+</button>
+//       </div>
+//       <div>
+//         <button onClick={() => setCount((c) => c - step)}>-</button>
+//         <span>Count:{count}</span>
+//         <button onClick={() => setCount((c) => c + step)}>+</button>
+//       </div>
+//       <p>
+//         <span>
+//           {count === 0
+//             ? `Today is:${date.toLocaleDateString()}`
+//             : count > 0
+//               ? `${count} days from today is:${date.toLocaleDateString()}`
+//               : `${Math.abs(count)} days ago was:${date.toLocaleDateString()}`}
+//         </span>
+//       </p>
+//     </div>
+//   );
+// }
+/////////////////////////////////////////second version////////////////
 
 export default function App() {
   return (
@@ -11,17 +50,30 @@ function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
   const date = new Date();
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
   date.setDate(date.getDate() + count);
   return (
     <div>
       <div>
-        <button onClick={() => setStep((c) => c - 1)}>-</button>
-        <span>Steps:{step}</span>
-        <button onClick={() => setStep((c) => c + 1)}>+</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>step:{step}</span>
       </div>
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count:{count}</span>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <p>
@@ -33,6 +85,11 @@ function Counter() {
               : `${Math.abs(count)} days ago was:${date.toLocaleDateString()}`}
         </span>
       </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
