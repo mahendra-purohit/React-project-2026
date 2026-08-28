20746c843463aa29a5ef53e2204306bd9aa3c853
mahendra-purohit <mhpurohit90@gmail.com>
2026-08-28 22:18:18 +0530

    commit

---
+++ usepopcorn/src/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+//import "./index.css";
+//import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setmovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        color="red"
+        maxRating={10}
+        onsetmovieRating={setmovieRating}
+      />
+      <p>You have {movieRating} rating</p>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/*<App />*/}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "bad", "okay", "good", "amazing"]}
+      defaultRating={3}
+    />
+    <Test />
+  </React.StrictMode>,
+);
